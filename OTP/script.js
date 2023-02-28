a04103aec8d358489c27ec33c40720facaec7c8f
sample <youngjae7981@gmail.com>
2023-02-28 17:32:37 +0900

    0228java

--- OTP/script.js
+++ OTP/script.js
@@ -1,41 +1,44 @@
-const inputs = document.querySelectorAll("input"),
-  button = document.querySelector("button");
+const inputs = document.querySelectorAll("input");
+const button = document.querySelector("button");
 
 inputs.forEach((input, index1) => {
   input.addEventListener("keyup", (e) => {
-    const currentInput = input,
-      nextInput = input.nextElementSibling,
-      prevInput = input.previousElementSibling;
+    const currentInput = input;
+    const nextInput = input.nextElementSibling;
+    const prevInput = input.previousElementSibling;
+
+    if (nextInput && nextInput.hasAttribute("disabled") && currentInput.value !== "") {
+      nextInput.removeAttribute("disabled");
+      nextInput.focus();
+    }
 
     if (currentInput.value.length > 1) {
       currentInput.value = "";
       return;
     }
 
-    if (
-      nextInput &&
-      nextInput.hasAttribute("disabled") &&
-      currentInput.value !== ""
-    ) {
-      nextInput.removeAttribute("disabled");
-      nextInput.focus();
-    }
-
     if (e.key === "Backspace") {
       inputs.forEach((input, index2) => {
-        if (index1 <= index2 && prevInput) {
+        if (index1 <= index2) {
           input.setAttribute("disabled", true);
           input.value = "";
-          prevInput.focus();
+          if (prevInput) {
+            prevInput.focus();
+          }
+        }
+        if (index1 === 0) {
+          inputs[0].removeAttribute("disabled");
+          inputs[0].focus();
         }
       });
     }
 
-    if (!inputs[3].disabled && inputs[3].value !== "") {
+    if (!inputs[inputs.length - 1].hasAttribute("disabled") && inputs[inputs.length - 1].value !== "") {
       button.classList.add("active");
-      return;
+    } else {
+      button.classList.remove("active");
     }
-    button.classList.remove("active");
+
   });
 });
 
